14acb79737f1f9259c2ad5061b06c3489c8b23f1
harshup4 <harshup4@gmail.com>
2026-07-17 13:40:14 +0530

    Third Commit

--- tests/FirstTestPO.spec.js
+++ tests/FirstTestPO.spec.js
@@ -4,7 +4,7 @@ const dataset = JSON.parse(JSON.stringify(require('../utils/placeorder_testdata.
 const dataset_array = JSON.parse(JSON.stringify(require('../utils/placeorder_testdata_array.json')));
 
 //test.describe.configure({mode:'parallel'});
-test.only('@Sanity First Test', async ({page})=>{
+test('@Sanity First Test', async ({page})=>{
     const ProductName = dataset.productName;
     const username = dataset.username;
     const password = dataset.password;
